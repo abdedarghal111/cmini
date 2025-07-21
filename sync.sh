d3870a7aa325718ce5eeebc3274562f5bcd8e545
antomfdez <antonio.mesa.fdez@gmail.com>
2025-07-21 23:24:05 +0200

    Fix checkout reference in sync script to use upstream/master

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git fetch origin
-git checkout origin/master -- authors.json corpora.json likes.json links.json layouts cache
+git checkout upstream/master -- authors.json corpora.json likes.json links.json layouts cache
 git commit -m "Sync data"
 git pull
 git push
