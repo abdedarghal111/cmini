bdfcb34b973b432290e2e45a851e300e7e519702
antomfdez <antonio.mesa.fdez@gmail.com>
2025-07-22 02:32:55 +0200

    Add git add command for specified files before commit in sync script

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 
 git fetch --all --prune
 git checkout upstream/master -- authors.json corpora.json likes.json links.json layouts cache
+git add authors.json corpora.json likes.json links.json layouts cache
 git commit -m "Sync data"
 git pull
 git push
